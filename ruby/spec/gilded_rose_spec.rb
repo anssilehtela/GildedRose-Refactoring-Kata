91ec5f0ef3c0ecbe087c9775f74f2ee02ae35338
Anssi <anssi@HD695-ANSSIL.local>
2021-03-14 18:52:25 +0200

    Added approvals and a base approval

---
+++ ruby/spec/gilded_rose_spec.rb
@@ -0,0 +1,46 @@
+#require File.join(File.dirname(__FILE__), 'gilded_rose')
+require_relative '../gilded_rose'
+require 'approvals/rspec'
+
+Approvals.configure do |config|
+  config.approvals_path = 'output/goes/here/'
+end
+
+describe GildedRose do
+
+  describe "#update_quality" do
+    xit "does not change the name" do
+      items = [Item.new("foo", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].name).to eq "fixme"
+    end
+  end
+
+  context 'approvals' do
+    before do 
+      items = [Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20),
+               Item.new(name="Aged Brie", sell_in=2, quality=0),
+               Item.new(name="Elixir of the Mongoose", sell_in=5, quality=7),
+               Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+               Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+               Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+               Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+               Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+               # This Conjured item does not work properly yet
+               Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
+              ]
+      @item_array = []
+      gilded_rose = GildedRose.new(items)
+      25.times do
+        gilded_rose.update_quality
+        @item_array << items.map { |i| i.to_s }
+      end
+    end
+    it 'works' do
+      verify do
+        @item_array
+      end
+    end
+  end
+
+end
