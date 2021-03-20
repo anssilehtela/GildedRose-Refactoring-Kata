094a78c7c1200c5401725c1af1b60cc07fc7d606
Anssi <anssi@HD695-ANSSIL.local>
2021-03-20 17:51:51 +0200

    Removing unnecessary ifs

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,49 +6,37 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
+      next if item.name == "Sulfuras, Hand of Ragnaros"
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
+          item.quality = item.quality - 1
         end
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
+        item.quality = item.quality + 1
+        if item.name == "Backstage passes to a TAFKAL80ETC concert"
+          if item.sell_in < 11
+            item.quality = item.quality + 1
+          end
+          if item.sell_in < 6
+            item.quality = item.quality + 1
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
+      item.sell_in = item.sell_in - 1
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
+              item.quality = item.quality - 1
             end
           else
             item.quality = item.quality - item.quality
           end
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          item.quality = item.quality + 1
         end
       end
+      item.quality = 50 if item.quality > 50
     end
   end
 end
--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -9,10 +9,14 @@ end
 describe GildedRose do
 
   describe "#update_quality" do
-    xit "does not change the name" do
+    it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      expect(items[0].name).to eq "foo"
+    end
+
+    context "When item is Aged Brie" do
+      let(:item){ Item.new("Aged Brie", 0, 0) }
     end
   end
 
