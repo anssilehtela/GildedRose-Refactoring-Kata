ebf88b680dbfb6607d5e191b255423ca1d00a2d2
Anssi <anssi@HD695-ANSSIL.local>
2021-03-20 22:17:56 +0200

    Added the conjured quality rule

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -10,7 +10,7 @@ class GildedRose
       case item.name
       when "Sulfuras, Hand of Ragnaros"
         next
-      when /Aged Brie/
+      when "Aged Brie"
         quality_change = 1
       when "Backstage passes to a TAFKAL80ETC concert"
         if item.sell_in < 1
@@ -29,6 +29,10 @@ class GildedRose
         quality_change = -1
       end
       quality_change = quality_change * 2 if item.sell_in < 1
+      #Would like to ask the owners how should stuff go with conjured Brie or tickets..
+      #Assuming the conjuration does not apply to those
+      quality_change = quality_change * 2 if conjured?(item) && quality_change < 0
+
       item.quality = item.quality + quality_change
       item.quality = 50 if item.quality > 50
       item.quality = 0 if item.quality < 0
@@ -36,6 +40,11 @@ class GildedRose
     end
   end
 end
+  
+  #Assuming this is the identification method, as the Item class cannot be touched
+  def conjured?(item)
+    item.name.start_with? "Conjured"
+  end
 
 class Item
   attr_accessor :name, :sell_in, :quality
